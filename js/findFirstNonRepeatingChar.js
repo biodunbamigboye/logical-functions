dca7d7e01cb21df9a68b2eaa8623f70d90a452ad
Boluwaji Akinsefunmi <bolu.akinsefunmi@gmail.com>
2021-04-14 16:23:36 +0100

    Update findFirstNonRepeatingChar.js
    Optimized function to traverse the string just once with a for-of loop

--- js/findFirstNonRepeatingChar.js
+++ js/findFirstNonRepeatingChar.js
@@ -1,19 +1,11 @@
 const findFirstNonRepeatingChar = (word) => {
     if (typeof word !== 'string') { return 'ony_string_allowed' }
-    let arr = word.split('');
-    for (let i = 0; i < arr.length; i++) {
-        let count = 0;
-        let data = arr[i];
-
-        for (let inner = 0; inner < arr.length; inner++) {
-            let dataInner = arr[inner]
-            if (dataInner == data) { count += 1 }
-
-        }
-
-        console.log(count)
-        if (count == 1) {
-            return data
+    let result;
+    for (const character of word) {
+        if (!(word.replace(character).includes(character))) {
+            result = character;
+            break;
         }
     }
+    return result
 }
