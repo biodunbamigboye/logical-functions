a91d50456a21d8c04b4acd596b705b9d9de593aa
mrwaccaff <bamigboyebiodun@gmail.com>
2021-04-19 09:31:15 +0100

    added a function to know prime numbers

---
+++ js/isPrimeNumber.js
@@ -0,0 +1,16 @@
+const isPrimeNumber = (number) => {
+        if (typeof number !== 'number' || number <= 1) return false;
+        for (var i = 2; i <= Math.floor(Math.sqrt(number)); i++) {
+            if (number % i === 0) return false;
+        }
+        return true;
+    }
+    /*
+    Author : wasp ix 
+
+    Contributor : Biodun Bamigboye
+
+    Definition : This function receives a  Number as argument and returns
+    true or false if its a prime number
+
+    */
