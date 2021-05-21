1315a4e8e8c806eaefd97774e774ea00449ac144
mrwaccaff <bamigboyebiodun@gmail.com>
2021-05-21 23:00:00 +0100

    Added a php function to generate random alphabet

---
+++ php/genRandomAlphabet.php
@@ -0,0 +1,20 @@
+<?php
+function genRandomAlphabet(Int $length=1) : String {
+$output = [];
+for ($i = 0; $i<$length;$i++)array_push($output,chr(rand(97,122)));
+return implode("",$output);
+}
+//-- Usage
+print_r(genRandomAlphabet(4));
+/*
+Author : Biodun Bamigboye
+
+Description
+This function generates a random alphabet,
+the length passed will be the length of the 
+return value e.g
+genRandomAlphabet(5)
+return value : "fhksi"
+
+*/
+
