21491ee8787c52cfa6ae834c22fc2a29261b0f53
mrwaccaff <bamigboyebiodun@gmail.com>
2021-05-25 20:31:48 +0100

    Adds a file to explain what its like working with object in php

---
+++ php/workingWithObject.php
@@ -0,0 +1,17 @@
+<?php 
+$object = json_encode([
+    'name' => 'Biodun Bamigboye',
+    'job_title' => 'lead developer'
+    ]);
+$userdataAsObject = json_decode($object);
+echo $userdataAsObject->name ;
+ /* return Value : Biodun Bamigboye */
+
+ //-- To convert it to an array
+ $userdataAsArray = json_decode($object,true);
+ echo $userdataAsArray['name'] ;
+ /* return Value : Biodun Bamigboye */
+
+ /* 
+Author : Biodun Bamigboye
+ */
