856576a2402522ca54267a55a277f316c827c88f
bidunbamigboye <bamigboyebiodun@gmail.com>
2023-01-28 10:48:53 +0100

    Added PHP function to get Current Day of the Week

---
+++ php/getCurrentDayOfWeek.php
@@ -0,0 +1,18 @@
+<?php
+
+function getCurrentDayOfWeek(bool $responseInWords): string|int
+{
+    $dayOfWeek = date('w');
+    $dayOfWeek = $dayOfWeek == 0 ? 7 : $dayOfWeek;
+    $daysInWord = [
+        1 => 'Monday',
+        2 => 'Tuesday',
+        3 => 'Wednesday',
+        4 => 'Thursday',
+        5 => 'Friday',
+        6 => 'Saturday',
+        7 => 'Sunday'
+    ];
+    
+    return $responseInWords ? $daysInWord[$dayOfWeek] : $dayOfWeek;
+}
