50a4e61e51d4dd2b6542a3c11c60e83cc3221bf9
bidunbamigboye <bamigboyebiodun@gmail.com>
2023-02-03 21:29:30 +0100

    Added Javascript Function to get Current Month of the Year

---
+++ js/getCurrentMonthOfYear.js
@@ -0,0 +1,11 @@
+function getCurrentMonthOfYear(responseInWords = false) {
+    var d = new Date();
+    var month = d.getMonth() + 1;
+
+    let monthNames = [
+        "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+    return responseInWords ? monthNames[month-1] : month;
+}
+
+console.log(getCurrentMonthOfYear());
