72336df6575e88e94649c1bf0a18ac0e25625f97
bidunbamigboye <bamigboyebiodun@gmail.com>
2023-02-08 21:03:37 +0100

    Updated Function Parameter Naming for Clarity

--- php/getNthDayOfYearByDate.php
+++ php/getNthDayOfYearByDate.php
@@ -1,10 +1,10 @@
 <?php
 
-function getNthDayOfYearByDay(int $year, int $month, string $dd): int
+function getNthDayOfYearByDate(int $year, int $month, string $day): int
 {
     $date = new DateTime();
     $date->setTime(0, 0, 0);
-    $date->setDate($year, $month, $dd);
+    $date->setDate($year, $month, $day);
     $startDate = (new DateTime())->setTime(0, 0, 0)->setDate($year, 1, 1);
     $interval = $date->diff($startDate);
     return $interval->days + 1;
