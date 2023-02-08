9bde02aefdfa17d2b8cb707b3d721ff11b1d1ed7
bidunbamigboye <bamigboyebiodun@gmail.com>
2023-02-08 21:12:34 +0100

    Added PHP Function to get Number of days Away from Today

---
+++ php/getNumberOfDaysAway.php
@@ -0,0 +1,11 @@
+<?php
+
+function getNumberOfDaysAwayFromToday(int $year, int $month, string $day): int 
+{
+    $date = new DateTime();
+    $date->setTime(0, 0, 0);
+    $date->setDate($year, $month, $day);
+    $today = (new DateTime())->setTime(0, 0, 0);
+    $interval = $date->diff($today);
+    return $interval->days;
+}
