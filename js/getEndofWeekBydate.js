d521b20762bfdaa8188c2b3d5b20524823f0e181
bidunbamigboye <bamigboyebiodun@gmail.com>
2023-02-14 11:47:28 +0100

    Added Function to Get end of Week by Date

---
+++ js/getEndofWeekBydate.js
@@ -0,0 +1,6 @@
+const getEndOfWeekByDate = (year, month, day) => {
+    const date = new Date(year, month-1, day);
+    const dayOfWeek = date.getDay();
+    const diff = date.getDate() - dayOfWeek + (dayOfWeek === 0 ? -6 : 1);
+    return new Date(date.setDate(diff + 6));
+}
