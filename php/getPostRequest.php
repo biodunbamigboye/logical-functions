aeca8d46ebcda99529693b4b2afc805c6a04ef75
Moffat Munene <moffmu@gmail.com>
2023-06-01 03:40:32 +0300

    use modern array push

--- php/getPostRequest.php
+++ php/getPostRequest.php
@@ -1,36 +1,37 @@
 <?php
-      function getPostRequest(Array $postKeys){
-        
-        $output=[];
-        $errorLog=[];
-        foreach ($postKeys as $key ) {   
-            if(!array_key_exists($key, $_POST)){
-            
-            array_push($errorLog,$key);
+function getPostRequest(array $postKeys): array
+{
+
+    $output = [];
+    $errorLog = [];
+    foreach ($postKeys as $key) {
+        if (!array_key_exists($key, $_POST)) {
+
+            $errorLog[] = $key;
             continue;
-            }
-            $data = $_POST[$key];
-             $output[$key]=$data;
-           }
-           return ['data' => $output, 'error' =>$errorLog];
-      }
+        }
+        $data = $_POST[$key];
+        $output[$key] = $data;
+    }
+    return ['data' => $output, 'error' => $errorLog];
+}
 
 
-    // -- Usage
-getPostRequest(['name','email','keyNotPosted']);
-// Return Value 
+// -- Usage
+getPostRequest(['name', 'email', 'keyNotPosted']);
+// Return Value
 [
-'data' => [
-    'name' => 'Biodun Bamigboye',
-    'email' => 'biodunbamigboye@eportalnet.com'
-],
-'error' => ['keyNotPosted']
+    'data' => [
+        'name' => 'Biodun Bamigboye',
+        'email' => 'biodunbamigboye@eportalnet.com'
+    ],
+    'error' => ['keyNotPosted']
 ];
-      
-/*
-Author : Biodun Bamigboye 
 
-This function receives an indexed array of post key indexes 
+/*
+Author : Biodun Bamigboye
+
+This function receives an indexed array of post key indexes
 and returns an associative array of entered data and index
 array for keys with no data existing.
 Note That data is subjected to further tests by you before
